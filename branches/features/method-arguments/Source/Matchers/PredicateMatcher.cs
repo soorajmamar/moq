198959da6c5b0ffc471ca5817e144efd0e622f58
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-01-31 11:51:36 +0000

    Branched to prototype method arguments on callbacks and returns.

---
+++ branches/features/method-arguments/Source/Matchers/PredicateMatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Moq
+{
+	internal class PredicateMatcher : IMatcher
+	{
+		Delegate predicate;
+
+		public void Initialize(Expression matcherExpression)
+		{
+			// TODO: validate argument or trust the compiler?
+			var call = matcherExpression as MethodCallExpression;
+			var lambda = call.Arguments[0].StripQuotes() as LambdaExpression;
+			predicate = lambda.Compile();
+		}
+
+		public bool Matches(object value)
+		{
+			return (bool)predicate.DynamicInvoke(value);
+		}
+	}
+}
---
+++ branches/features/method-arguments/Source/MethodCall.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using Castle.Core.Interceptor;
+
+namespace Moq
+{
+	internal class MethodCall : ICall, IProxyCall
+	{
+		Expression originalExpression;
+		MethodInfo method;
+		Exception exception;
+		Action callback;
+		IMatcher[] argumentMatchers;
+
+		public MethodCall(Expression originalExpression, MethodInfo method, params Expression[] arguments)
+		{
+			this.originalExpression = originalExpression;
+			this.method = method;
+			this.argumentMatchers = (from expr in arguments
+							   select MatcherFactory.CreateMatcher(expr)).ToArray();
+		}
+
+		public bool IsVerifiable { get; set; }
+		public bool Invoked { get; set; }
+		public Expression ExpectExpression { get { return originalExpression; } }
+
+		public void Throws(Exception exception)
+		{
+			this.exception = exception;
+		}
+
+		public ICall Callback(Action callback)
+		{
+			this.callback = callback;
+			return this;
+		}
+
+		public ICall Verifiable()
+		{
+			IsVerifiable = true;
+
+			return this;
+		}
+
+		public bool Matches(IInvocation call)
+		{
+			if (call.Method == method &&
+				argumentMatchers.Length == call.Arguments.Length)
+			{
+				for (int i = 0; i < argumentMatchers.Length; i++)
+				{
+					if (!argumentMatchers[i].Matches(call.Arguments[i]))
+						return false;
+				}
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public virtual void Execute(IInvocation call)
+		{
+			Invoked = true;
+
+			if (callback != null)
+				callback();
+
+			if (exception != null)
+				throw exception;
+		}
+	}
+}
