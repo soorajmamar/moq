e78a6362244d77f3fdfb52d5827fb28ab2847f12
bcardiff <bcardiff@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-02-04 23:05:36 +0000

    Fix MockFixture null pointer. <Mocked,Mock> dictionary implemented. If GetHashCode is not invariant, it fails.

---
+++ branches/private/bcardiff/MockedByDictionary/Source/ReferenceEqualsComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moq
+{
+	internal class ReferenceEqualsComparer : IEqualityComparer<object>
+	{
+		bool IEqualityComparer<object>.Equals(object x, object y)
+		{
+			return object.ReferenceEquals(x, y);
+		}
+
+		int IEqualityComparer<object>.GetHashCode(object obj)
+		{
+			return obj.GetHashCode();
+		}
+	}
+}
