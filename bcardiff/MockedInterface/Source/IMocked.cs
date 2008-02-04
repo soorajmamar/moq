7337b96397a5d3521522f2d1f3ac838525970112
bcardiff <bcardiff@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-02-04 22:39:29 +0000

    Mocked support by interface and Interceptor. All except MBRO.

---
+++ bcardiff/MockedInterface/Source/IMocked.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moq
+{
+	/// <summary>
+	/// Implemented by all moked object.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IMocked<T>
+		where T : class
+	{
+		/// <summary>
+		/// Reference the Mock that contains this as the Object.
+		/// </summary>
+		Mock<T> Mock { get; }
+	}
+}
