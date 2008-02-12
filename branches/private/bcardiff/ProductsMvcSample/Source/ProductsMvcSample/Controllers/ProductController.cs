b5d09defd3217dd86a8273f776663b95ac2a6136
bcardiff <bcardiff@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-02-12 12:45:09 +0000

    Product MVC sample. required import to use svn:externals

---
+++ branches/private/bcardiff/ProductsMvcSample/Source/ProductsMvcSample/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductsMvcSample.Controllers
+{
+	public class ProductController : Controller
+	{
+
+		[ControllerAction]
+		public void Index()
+		{
+		}
+
+		[ControllerAction]
+		public void Category(int id)
+		{
+		}
+	}
+}
