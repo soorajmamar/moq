df1128a5158759865544699b58bd4a2ede0cdf20
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-03-10 16:58:33 +0000

    Tagged v2.0 release

---
+++ tags/2.0/Source/Language/Flow/IExpect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moq.Language.Flow
+{
+	/// <summary>
+	/// Implements the fluent API.
+	/// </summary>
+	public interface IExpect : ICallback, IThrowsOnceVerifies, IHideObjectMembers
+	{
+	}
+
+	/// <summary>
+	/// Implements the fluent API.
+	/// </summary>
+	public interface IExpect<TResult> : ICallback<TResult>, IReturnsThrows<TResult>, IHideObjectMembers
+	{
+	}
+}
