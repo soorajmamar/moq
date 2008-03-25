e0807430f967a7be040ffd22d6968ad518bd1f9f
bcardiff <bcardiff@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-03-25 16:41:17 +0000

    custom matchers idea. (not working)

---
+++ bcardiff/CustomMatchers/UnitTests/CustomMatcherFixture.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Moq.Tests
+{
+	[TestFixture]
+	public class CustomMatcherFixture
+	{
+		public interface IFoo
+		{
+			void Bar(string p);
+		}
+
+		[Test]
+		public void Foo()
+		{
+			var mock = new Mock<IFoo>();
+			mock.Expect(x => x.Bar(
+				It.Is<string>(a => IsMagicString(a))
+			));
+			mock.Object.Bar("magic");
+		}
+
+		[Test]
+		public void ShouldTranslateToUseMatcherImplementation()
+		{
+			var mock = new Mock<IFoo>();
+			mock.Expect(x => x.Bar(IsMagicString()));
+			mock.Object.Bar("magic");
+		}
+
+		[Test]
+		[ExpectedException]
+		public void ShouldTranslateToUseMatcherImplementation2()
+		{
+			var mock = new Mock<IFoo>();
+			mock.Expect(x => x.Bar(IsMagicString()));
+			mock.Object.Bar("no-magic");
+		}
+
+		[CustomMatcher]
+		public static string IsMagicString() { return null; }
+		public static bool IsMagicString(string arg)
+		{
+			return arg == "magic";
+		}
+	}
+}
