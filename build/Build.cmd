893b30801343675374b7743d511a92976f0c07f5
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-05-25 08:45:36 +0000

    updated script to include Moq.Contrib

--- build/Build.cmd
+++ build/Build.cmd
@@ -1,4 +1,6 @@
 msbuild ..\trunk\Source\Moq.csproj /p:Configuration=Release
+msbuild ..\trunk\Extensions\Contrib\Source\Moq.Contrib.csproj /p:Configuration=Release
 copy ..\trunk\Source\bin\Release\Moq.* ..\drops\
+copy ..\trunk\Extensions\Contrib\Source\bin\Release\Moq.Contrib.* ..\drops\
 "Sandcastle Help File Builder\SandcastleBuilderConsole.exe" ..\trunk\Moq.shfb -include=Moq,MockBehavior -exclude=Moq,MockBehavior,Normal -exclude=Moq,MockBehavior,Relaxed -exclude=Moq,IMock
 copy Help\Moq.chm ..\drops\Moq.chm /Y
