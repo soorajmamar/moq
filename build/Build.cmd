cd14768439cb9bde4cafc78290ca73cd4707dfd2
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-07-07 02:56:44 +0000

    Fixed build batch and help generator.

--- build/Build.cmd
+++ build/Build.cmd
@@ -1,6 +1,6 @@
-msbuild ..\trunk\Extensions\Contrib\Source\Moq.Contrib.csproj /p:Configuration=Release
+msbuild ..\trunk\Source\Moq.csproj /p:Configuration=Release
 
-xcopy ..\trunk\Extensions\Contrib\Source\bin\Release\Moq*.* ..\drops\Current\ /Y
+xcopy ..\trunk\Source\bin\Release\Moq*.* ..\drops\Current\ /Y
 "Sandcastle Help File Builder\SandcastleBuilderConsole.exe" ..\trunk\Moq.shfb -include=Moq,MockBehavior -exclude=Moq,MockBehavior,Normal -exclude=Moq,MockBehavior,Relaxed -exclude=Moq,IMocked
 
 xcopy Help\Moq.chm ..\drops\Current\ /Y 
