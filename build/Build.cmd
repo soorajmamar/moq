43a48a8799e8504fbe82a9131cd7774e8b5b07de
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2008-07-09 05:46:30 +0000

    Updated build script

--- build/Build.cmd
+++ build/Build.cmd
@@ -1,6 +1,10 @@
 msbuild ..\trunk\Source\Moq.csproj /p:Configuration=Release
 
 xcopy ..\trunk\Source\bin\Release\Moq*.* ..\drops\Current\ /Y
+xcopy ..\trunk\License.txt "..\drops\Current\" /Y /I
+pushd ..\drops\Current\
+ren License.txt "Moq License.txt" 
+popd
 "Sandcastle Help File Builder\SandcastleBuilderConsole.exe" ..\trunk\Moq.shfb -include=Moq,MockBehavior -exclude=Moq,MockBehavior,Normal -exclude=Moq,MockBehavior,Relaxed -exclude=Moq,IMocked
 
 xcopy Help\Moq.chm ..\drops\Current\ /Y 
