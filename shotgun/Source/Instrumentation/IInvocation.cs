43958819b886c0be2a8ebaf99bc597129e40358d
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-01-03 21:27:25 +0000

    WIP

---
+++ shotgun/Source/Instrumentation/IInvocation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Moq.Instrumentation
+{
+	/// <summary>
+	/// Represents an invocation to an instrumented/proxied member.
+	/// </summary>
+	public interface IInvocation
+	{
+		/// <summary>
+		/// The target object where the invocation is being performed.
+		/// </summary>
+		object Target { get; set; }
+		/// <summary>
+		/// The type of the target object being invoked. Needed as 
+		/// the <see cref="Target"/> may be null for static invocations.
+		/// </summary>
+		Type TargetType { get; set; }
+		/// <summary>
+		/// The member being invoked.
+		/// </summary>
+		MethodBase Method { get; set; }
+		/// <summary>
+		/// The arguments of the invocation (regular, ref and out).
+		/// </summary>
+		object[] Arguments { get; set; }
+		/// <summary>
+		/// The return value of the methor invocation, if any.
+		/// </summary>
+		object ReturnValue { get; set; }
+		/// <summary>
+		/// Whether the actual method should be invoked or not.
+		/// </summary>
+		bool ShouldContinue { get; set; }
+	}
+}
