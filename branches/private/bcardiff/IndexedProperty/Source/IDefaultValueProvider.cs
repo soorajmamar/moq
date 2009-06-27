adb42a742c08c1bfe50cc5de8387b0ef66e17807
bcardiff <bcardiff@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-06-27 04:32:46 +0000

    branch IndexedProperty created

---
+++ branches/private/bcardiff/IndexedProperty/Source/IDefaultValueProvider.cs
@@ -0,0 +1,62 @@
+﻿//Copyright (c) 2007, Moq Team 
+//http://code.google.com/p/moq/
+//All rights reserved.
+
+//Redistribution and use in source and binary forms, 
+//with or without modification, are permitted provided 
+//that the following conditions are met:
+
+//    * Redistributions of source code must retain the 
+//    above copyright notice, this list of conditions and 
+//    the following disclaimer.
+
+//    * Redistributions in binary form must reproduce 
+//    the above copyright notice, this list of conditions 
+//    and the following disclaimer in the documentation 
+//    and/or other materials provided with the distribution.
+
+//    * Neither the name of the Moq Team nor the 
+//    names of its contributors may be used to endorse 
+//    or promote products derived from this software 
+//    without specific prior written permission.
+
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+//CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+//INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+//MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+//CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+//INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+//WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+//NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+//SUCH DAMAGE.
+
+//[This is the BSD license, see
+// http://www.opensource.org/licenses/bsd-license.php]
+
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Moq
+{
+	/// <summary>
+	/// Interface to be implemented by classes that determine the 
+	/// default value of non-expected invocations.
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Advanced)]
+	internal interface IDefaultValueProvider
+	{
+		/// <summary>
+		/// Provides a value for the given member and arguments.
+		/// </summary>
+		/// <param name="member">The member to provide a default 
+		/// value for.</param>
+		/// <param name="arguments">Optional arguments passed in 
+		/// to the call that requires a default value.</param>
+		object ProvideDefault(MethodInfo member, object[] arguments);
+	}
+}
---
+++ branches/private/bcardiff/IndexedProperty/UnitTests/MockDefaultValueProviderFixture.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Moq.Tests
+{
+	public class MockDefaultValueProviderFixture
+	{
+		[Fact]
+		public void ProvidesMockValue()
+		{
+			var mock = new Mock<IFoo>();
+			var provider = new MockDefaultValueProvider(mock);
+
+			var value = provider.ProvideDefault(typeof(IFoo).GetProperty("Bar").GetGetMethod(), new object[0]);
+
+			Assert.NotNull(value);
+			Assert.True(value is IMocked);
+		}
+
+		[Fact]
+		public void CachesProvidedValue()
+		{
+			var mock = new Mock<IFoo>();
+			var provider = new MockDefaultValueProvider(mock);
+
+			var value1 = provider.ProvideDefault(typeof(IFoo).GetProperty("Bar").GetGetMethod(), new object[0]);
+			var value2 = provider.ProvideDefault(typeof(IFoo).GetProperty("Bar").GetGetMethod(), new object[0]);
+
+			Assert.Same(value1, value2);
+		}
+
+		[Fact]
+		public void ProvidesEmptyValueIfNotMockeable()
+		{
+			var mock = new Mock<IFoo>();
+			var provider = new MockDefaultValueProvider(mock);
+
+			var value = provider.ProvideDefault(typeof(IFoo).GetProperty("Value").GetGetMethod(), new object[0]);
+			Assert.Equal(default(string), value);
+
+			value = provider.ProvideDefault(typeof(IFoo).GetProperty("Value").GetGetMethod(), new object[0]);
+			Assert.Equal(default(string), value);
+
+			value = provider.ProvideDefault(typeof(IFoo).GetProperty("Indexes").GetGetMethod(), new object[0]);
+			Assert.True(value is IEnumerable<int> && ((IEnumerable<int>)value).Count() == 0);
+
+			value = provider.ProvideDefault(typeof(IFoo).GetProperty("Bars").GetGetMethod(), new object[0]);
+			Assert.True(value is IBar[] && ((IBar[])value).Length == 0);
+		}
+
+		[Fact]
+		public void NewMocksHaveSameBehaviorAndDefaultValueAsOwner()
+		{
+			var mock = new Mock<IFoo>();
+			var provider = new MockDefaultValueProvider(mock);
+
+			var value = provider.ProvideDefault(typeof(IFoo).GetProperty("Bar").GetGetMethod(), new object[0]);
+
+			var barMock = Mock.Get((IBar)value);
+
+			Assert.Equal(mock.Behavior, barMock.Behavior);
+			Assert.Equal(mock.DefaultValue, barMock.DefaultValue);
+		}
+
+		[Fact]
+		public void CreatedMockIsVerifiedWithOwner()
+		{
+			var mock = new Mock<IFoo>();
+			var provider = new MockDefaultValueProvider(mock);
+
+			var value = provider.ProvideDefault(typeof(IFoo).GetProperty("Bar").GetGetMethod(), new object[0]);
+
+			var barMock = Mock.Get((IBar)value);
+			barMock.Setup(b => b.Do()).Verifiable();
+
+			Assert.Throws<MockVerificationException>(() => mock.Verify());
+		}
+
+		public interface IFoo
+		{
+			IBar Bar { get; set; }
+			string Value { get; set; }
+			IEnumerable<int> Indexes { get; set; }
+			IBar[] Bars { get; set; }
+		}
+
+		public interface IBar { void Do(); }
+	}
+}
