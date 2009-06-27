6f974b03ec3cf0d9f28e9d2e453722c2d60bc923
bcardiff <bcardiff@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-06-27 18:49:48 +0000

    IndexedProperty working for any single and double keyed properties.

--- branches/private/bcardiff/IndexedProperty/UnitTests/IndexedPropertyFixture.cs
+++ branches/private/bcardiff/IndexedProperty/UnitTests/IndexedPropertyFixture.cs
@@ -15,6 +15,11 @@ namespace Moq.Tests
 			Bar this[int key1, int key2] { get; set; }
 		}
 
+		public interface INotSupportedFoo
+		{
+			Bar this[int key1, int key2, int key3] { get; set; }
+		}
+
 		public class Bar { }
 
 		Bar bar;
@@ -26,6 +31,7 @@ namespace Moq.Tests
 			m = new Mock<IFoo>(MockBehavior.Strict);
 		}
 
+		#region Single indexed property
 		[Fact]
 		public void AllowGetByExactMatch()
 		{
@@ -131,6 +137,123 @@ namespace Moq.Tests
 			m.SetupProperty(x => x[It.IsAny<string>()]);
 			Assert.Throws<MockException>(() => m.VerifySet(x => x["key"]));
 			Assert.Throws<MockException>(() => m.VerifySet(x => x["key"] = bar));
+		}
+		#endregion
+
+		#region Double indexed property
+		[Fact]
+		public void DIP_AllowGetByExactMatch()
+		{
+			m.SetupGet(x => x[0, 0]).Returns(bar);
+			Assert.Equal(bar, m.Object[0, 0]);
+		}
+
+		[Fact]
+		public void DIP_FailGetNotMatchingIndex()
+		{
+			m.SetupGet(x => x[0, 0]).Returns(bar);
+			Assert.Throws<MockException>(() => { var r = m.Object[1, 1]; });
+		}
+
+		[Fact]
+		public void DIP_AllowSetByExactMatch()
+		{
+			m.SetupSet(x => x[0, 0]);
+			m.Object[0, 0] = bar;
+		}
+
+		[Fact]
+		public void DIP_FailSetNotMatchingIndex()
+		{
+			m.SetupSet(x => x[0, 0]);
+			Assert.Throws<MockException>(() => { m.Object[1, 1] = bar; });
+		}
+
+		[Fact]
+		public void DIP_AllowSetForAny()
+		{
+			m.SetupSet(x => x[It.IsAny<int>(), It.IsAny<int>()]);
+			m.Object[0, 0] = null;
+			m.Object[0, 1] = null;
+		}
+
+		[Fact]
+		public void DIP_SetupIndexedPropertyAllowGetAndSet()
+		{
+			m.SetupProperty(x => x[It.IsAny<int>(), It.IsAny<int>()]);
+			m.Object[0, 0] = bar;
+			Assert.Equal(bar, m.Object[0, 0]);
+		}
+
+		[Fact]
+		public void DIP_SetupIndexedPropertyKeyAllowGetAndSet()
+		{
+			m.SetupProperty(x => x[0, 0]);
+			m.Object[0, 0] = bar;
+			Assert.Equal(bar, m.Object[0, 0]);
+		}
+
+		[Fact]
+		public void DIP_SetupIndexedPropertyKeyFailSet()
+		{
+			m.SetupProperty(x => x[0, 0]);
+			Assert.Throws<MockException>(() => { m.Object[1, 1] = bar; });
+		}
+
+		[Fact]
+		public void DIP_SetupIndexedPropertyKeyFailGet()
+		{
+			m.SetupProperty(x => x[0, 0]);
+			Assert.Throws<MockException>(() => { var r = m.Object[1, 1]; });
+		}
+
+		[Fact]
+		public void DIP_SetupIndexedPropertyKeyAllowGetAndSetOnIntKeyedProperties()
+		{
+			m.SetupProperty(x => x[It.IsAny<int>(), It.IsAny<int>()]);
+			m.Object[2, 2] = bar;
+			Assert.Equal(bar, m.Object[2, 2]);
+		}
+
+		[Fact]
+		public void DIP_ShouldVerifyGet()
+		{
+			m.SetupProperty(x => x[It.IsAny<int>(), It.IsAny<int>()]);
+			m.Object[0, 0] = bar;
+			var r = m.Object[0, 0];
+			m.VerifyGet(x => x[0, 0]);
+		}
+
+		[Fact]
+		public void DIP_ShouldNotVerifyGet()
+		{
+			m.SetupProperty(x => x[It.IsAny<int>(), It.IsAny<int>()]);
+			Assert.Throws<MockException>(() => m.VerifyGet(x => x[1, 1]));
+		}
+
+		[Fact]
+		public void DIP_ShouldVerifySet()
+		{
+			m.SetupProperty(x => x[It.IsAny<int>(), It.IsAny<int>()]);
+			m.Object[0, 0] = bar;
+			m.VerifySet(x => x[0, 0] = bar);
+			m.VerifySet(x => x[0, 0]);
+		}
+
+		[Fact]
+		public void DIP_ShouldNotVerifySet()
+		{
+			m.SetupProperty(x => x[It.IsAny<int>(), It.IsAny<int>()]);
+			Assert.Throws<MockException>(() => m.VerifySet(x => x[1, 1]));
+			Assert.Throws<MockException>(() => m.VerifySet(x => x[1, 1] = bar));
+		}
+		#endregion
+
+		[Fact]
+		public void MoreThanTwoIndexesAreNotSupported()
+		{
+			var m = new Mock<INotSupportedFoo>();
+			Assert.Throws<NotSupportedException>(() => m.SetupProperty(x => x[It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>()]));
 		}
 	}
 }
