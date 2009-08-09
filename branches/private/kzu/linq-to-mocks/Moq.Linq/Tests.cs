2b6e5885d5fd5129086d9bd2a9f836b10947c761
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-08-09 22:35:27 +0000

    Added AsMock<T> extension methods on fluent API and system.object (alternative to Mock.Get)

--- branches/private/kzu/linq-to-mocks/Moq.Linq/Tests.cs
+++ branches/private/kzu/linq-to-mocks/Moq.Linq/Tests.cs
@@ -10,71 +10,30 @@ using System.Diagnostics;
 
 namespace Moq.Linq
 {
-	public static class AsMockExtensions
-	{
-		public static TMock AsMocked<TMock>(this IReturnsResult<TMock> returns)
-			where TMock : class
-		{
-			return returns.AsMock().Object;
-		}
-
-		public static Mock<TMock> AsMock<TMock>(this IReturnsResult<TMock> returns)
-			where TMock : class
-		{
-			// Make this properly supported.
-			return (Mock<TMock>)returns
-				.GetType()
-				.GetField("mock", BindingFlags.Instance | BindingFlags.NonPublic)
-				.GetValue(returns);
-		}
-
-		public static Mock<T> AsMock<T>(this T @object)
-			where T : class
-		{
-			return Mock.Get(@object);
-		}
-	}
-
 	public class Tests
 	{
 		public void TranslateToFluentMocks()
 		{
-			var fooMock = new Mock<IFoo>(MockBehavior.Strict);
-			fooMock.Setup(f => f.Bar.Baz("hey").Value).Returns(5);
-
 			var expression = ToExpression<IFoo, int>(f => f.Bar.Baz("hey").Value);
 
-			var factory = new MockFactory(MockBehavior.Strict);
-			var baz = factory
-				.Create<IBaz>()
-				.Setup(z => z.Value)
-				.Returns(5)
-				.AsMocked();
+			//// For linq querying we need something different.
+			//factory
+			//    .Create<IBar>()
+			//    .Setup(b => b.Baz("hey"))
+			//    .Returns(
+			//        factory.Create<IBaz>()
+			//        .Setup(z => z.Value)
+			//        .Returns(5)
+			//        .AsMocked()
+			//    )
+			//    .AsMocked();
 
-			var bar = factory
-				.Create<IBar>()
-				.Setup(b => b.Baz("hey"))
-				.Returns(baz)
-				.AsMocked();
+			//var value = foo.Bar.Baz("hey").Value;
 
-			fooMock.Setup(f => f.Bar).Returns(bar);
-
-			// For linq querying we need something different.
-			factory
-				.Create<IBar>()
-				.Setup(b => b.Baz("hey"))
-				.Returns(
-					factory.Create<IBaz>()
-					.Setup(z => z.Value)
-					.Returns(5)
-					.AsMocked()
-				)
-				.AsMocked();
-
-			var value = foo.Bar.Baz("hey").Value;
-
-			Debug.Assert(value == 5);
+			//Debug.Assert(value == 5);
 		}
+
+		
 
 		private LambdaExpression ToExpression<T1, T2>(Expression<Func<T1, T2>> expression)
 		{
