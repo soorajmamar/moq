e5504744b96a20e4e725b96f6eae5482a208ac46
bcardiff <bcardiff@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2009-11-21 18:58:49 +0000

    sample of how to use ConditionalSetups to implement sequential returns of the same method.

---
+++ branches/private/bcardiff/ConditionalSetups/UnitTests/ReturnSequenceHelperFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Moq.Tests
+{
+	public class ReturnSequenceHelperFixture
+	{
+		[Fact]
+		public void PerformSequence()
+		{
+			var mock = new Mock<IFoo>();
+
+			mock.SetupSequentials(x => x.Do())
+				.Returns(2)
+				.Returns(3)
+				.Throws<InvalidOperationException>();
+
+			Assert.Equal(2, mock.Object.Do());
+			Assert.Equal(3, mock.Object.Do());
+			Assert.Throws<InvalidOperationException>(() => { 
+				mock.Object.Do(); 
+			});
+		}
+
+		public interface IFoo
+		{
+			int Do();
+		}
+	}
+}
