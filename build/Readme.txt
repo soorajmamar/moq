05264ac67ca70528778365fe0f4be4491ab2c7c2
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2010-08-11 02:27:07 +0000

    Updated build script and help.

--- build/Readme.txt
+++ build/Readme.txt
@@ -1,5 +1,5 @@
 To regenerate the help file and site:
 1) Install Sandcastle: http://www.microsoft.com/downloads/details.aspx?FamilyId=E82EA71D-DA89-42EE-A715-696E3A4873B2&displaylang=en
 2) Install the Sandcastle Help File Builder: http://www.codeplex.com/SHFB/
-3) Copy both from the install location inside the moq\build folder.
+3) Copy both from the install location inside the moq\build folder (to build\Sandcastle and build\Sandcastle Help File Builder)
 3) Run Build.cmd
--- build/drop.cmd
+++ build/drop.cmd
@@ -1,8 +1,12 @@
 msbuild ..\trunk\Source\Moq.csproj /t:Rebuild /p:Configuration=Release /p:TargetFrameworkVersion=v3.5
+del ..\trunk\Source\bin\Release\Moq.dll.* /Y
 xcopy ..\trunk\Source\bin\Release\*.* ..\drops\Current\NET35 /Y /I
+
 msbuild ..\trunk\Source.Silverlight\Moq.Silverlight.csproj /t:Rebuild /p:Configuration=Release
 xcopy ..\trunk\Source.Silverlight\bin\Release\*.* ..\drops\Current\Silverlight4 /Y /I
+
 msbuild ..\trunk\Source\Moq.csproj /t:Rebuild /p:Configuration=Release
+del ..\trunk\Source\bin\Release\Moq.dll.* /Y
 xcopy ..\trunk\Source\bin\Release\*.* ..\drops\Current\NET40 /Y /I
 
 xcopy ..\trunk\License.txt "..\drops\Current\" /Y /I
