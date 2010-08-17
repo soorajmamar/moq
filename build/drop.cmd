bed1644b3869f56956c8d6ef546c018cafe8ea9f
kzu.net <kzu.net@b33fba48-7441-0410-8d5c-f397f7ceaa6c>
2010-08-17 14:35:45 +0000

    Updated version number for drop. Included non-ILMerged version of Moq for issue #108

--- build/drop.cmd
+++ build/drop.cmd
@@ -1,13 +1,17 @@
 msbuild ..\trunk\Source\Moq.csproj /t:Rebuild /p:Configuration=Release /p:TargetFrameworkVersion=v3.5
 del ..\trunk\Source\bin\Release\Moq.dll.* /Y
-xcopy ..\trunk\Source\bin\Release\*.* ..\drops\Current\NET35 /Y /I
+xcopy ..\trunk\Source\bin\Release\Moq.* ..\drops\Current\NET35 /Y /I
 
 msbuild ..\trunk\Source.Silverlight\Moq.Silverlight.csproj /t:Rebuild /p:Configuration=Release
 xcopy ..\trunk\Source.Silverlight\bin\Release\*.* ..\drops\Current\Silverlight4 /Y /I
 
 msbuild ..\trunk\Source\Moq.csproj /t:Rebuild /p:Configuration=Release
 del ..\trunk\Source\bin\Release\Moq.dll.* /Y
-xcopy ..\trunk\Source\bin\Release\*.* ..\drops\Current\NET40 /Y /I
+xcopy ..\trunk\Source\bin\Release\Moq.* ..\drops\Current\NET40 /Y /I
+
+msbuild ..\trunk\Source\Moq.csproj /t:Rebuild /p:Configuration=Release /p:MergeReferences=false
+del ..\trunk\Source\bin\Release\Moq.dll.* /Y
+xcopy ..\trunk\Source\bin\Release\Moq.* ..\drops\Current\NET40-NoCastle /Y /I
 
 xcopy ..\trunk\License.txt "..\drops\Current\" /Y /I
 msbuild ..\trunk\Moq.shfbproj
